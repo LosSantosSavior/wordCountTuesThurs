a0d7ebdf34abece7934e89130213990895568622
LosSantosSavior <nickrussell001@gmail.com>
2021-11-30 10:43:39 -0500

    Initial commit, 11/30/21 10:43AM

---
+++ src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    var fileLoc = Paths.get("warAndPeace.txt");
+	    List<String> allLines = null;
+	    try {
+	        allLines = Files.readAllLines(fileLoc);
+        } catch (IOException e)  {
+            System.out.println("Error reading file...");
+        }
+	    if (allLines == null)
+            System.exit(-1);
+    }
+}
