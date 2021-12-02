80c1d5d99227cf5a6fb63cdd2d5bc5e424d7030e
LosSantosSavior <nickrussell001@gmail.com>
2021-12-02 10:11:36 -0500

    hashmap added, 12/2/21 10:11AM

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,15 @@ package com.company;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
 
 public class Main {
 
     public static void main(String[] args) {
-	    var fileLoc = Paths.get("warAndPeace.txt");
+	    var wordCounts = new HashMap<String, Integer>(2048);
+        var fileLoc = Paths.get("warAndPeace.txt");
 	    List<String> allLines = null;
 	    try {
 	        allLines = Files.readAllLines(fileLoc);
@@ -17,5 +20,16 @@ public class Main {
         }
 	    if (allLines == null)
             System.exit(-1);
+	    for (var line: allLines)    {
+	        var allWords = line.split(" ");
+	        for (var word: allWords)    {
+	            word = word.replaceAll("\\p{Punct}", "");
+	            var lowerCaseWord = word.toLowerCase(Locale.ROOT);
+	            if (wordCounts.containsKey(lowerCaseWord))  {
+	               var currentCount = wordCounts.get(lowerCaseWord);
+	               wordCounts.replace(lowerCaseWord, ++currentCount);
+                }
+            }
+        }
     }
 }
